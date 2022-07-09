eec251d018912e00c38d00fc04a75c9af5b0dab5
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-07-09 10:06:53 +0100

    setup page and components

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import './sidebar.scss';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import StoreIcon from '@mui/icons-material/Store';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import InsertChartIcon from '@mui/icons-material/InsertChart';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <div className="top">
+                <span className="logo">lamadmin</span>
+            </div>
+            <hr />
+            <div className="center">
+                <ul>
+                    <p className="title">MAIN</p>
+                    <li>
+                        <DashboardIcon />
+                        <span>Dashboard</span>
+                    </li>
+                    <p className="title">LISTS</p>
+                    <li>
+                        <PersonOutlineIcon />
+                        <span>Users</span>
+                    </li>
+                    <li>
+                        <StoreIcon />
+                        <span>Products</span>
+                    </li>
+                    <li>
+                        <CreditCardIcon />
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <LocalShippingIcon />
+                        <span>Stats</span>
+                    </li>
+                    <p className="title">USEFUL</p>
+                    <li>
+                        <InsertChartIcon />
+                        <span>Delivery</span>
+                    </li>
+                    <li>
+                        <NotificationsNoneIcon />
+                        <span>Notifications</span>
+                    </li>
+                    <li>
+                        <SettingsSystemDaydreamOutlinedIcon />
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <PsychologyIcon />
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <SettingsApplicationsIcon />
+                        <span>Settings</span>
+                    </li>
+                    <p className="title">USER</p>
+                    <li>
+                        <AccountCircleOutlinedIcon />
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <LogoutIcon />
+                        <span>Logout</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="bottom">color options</div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.scss';
+import Sidebar from '../../components/sidebar/Sidebar';
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <Sidebar />
+            <div className='homeContainer'>container</div>
+        </div>
+    )
+}
+
+export default Home
