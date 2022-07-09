ce10d713aea526a886325941090d7bf21588c05c
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-07-09 14:34:28 +0100

    added top menu list

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 import './navbar.scss';
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import { NotificationsNoneOutlined, FullscreenExitOutlined, ListOutlined, ChatBubbleOutlineOutlined } from '@mui/icons-material';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='navbar'>
+            <div className="wrapper">
+                <div className="search">
+                    <input type="text" placeholder='Search...' />
+                    <SearchOutlinedIcon />
+                </div>
+                <div className="items">
+                    <div className="item">
+                        <LanguageOutlinedIcon className="icon" />
+                        English
+                    </div>
+                    <div className="item">
+                        <DarkModeOutlinedIcon className="icon" />
+                    </div>
+                    <div className="item">
+                        <FullscreenExitOutlined className="icon" />
+                    </div>
+                    <div className="item">
+                        <NotificationsNoneOutlined className="icon" />
+                        <div className='counter'>1</div>
+                    </div>
+                    <div className="item">
+                        <ChatBubbleOutlineOutlined className="icon" />
+                        <div className='counter'>2</div>
+                    </div>
+                    <div className="item">
+                        <ListOutlined className="icon" />
+                    </div>
+                    <div className="item">
+                        <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=50" alt="some image" className="avatar" />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './home.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
-
+import Navbar from '../../components/navbar/Navbar';
+import Widget from '../../components/widget/Widget';
 const Home = () => {
     return (
         <div className='home'>
             <Sidebar />
-            <div className='homeContainer'>container</div>
+            <div className='homeContainer'>
+                <Navbar />
+                <div className="widgets">
+                    <Widget type="user" />
+                    <Widget type="order" />
+                    <Widget type="earning" />
+                    <Widget type="balance" />
+                </div>
+            </div>
         </div>
     )
 }
