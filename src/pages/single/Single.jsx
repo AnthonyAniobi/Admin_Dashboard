d02be265ad81bde0760f72c9b63a8d3b977c0db3
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-07-14 08:17:09 +0100

    working on single page

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -11,10 +11,19 @@ const Single = () => {
                 <Navbar />
                 <div className="top">
                     <div className="left">
+                        <div className="editButton">Edit</div>
                         <h1 className="title">Information</h1>
                         <div className="item">
                             <img src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" className="itemImg" />
-                            details
+                            <div className="details">
+                                <h1 className="itemTile">
+                                    Jane Doe
+                                </h1>
+                                <div className="detailItem">
+                                    <div className="itemKey">Email: </div>
+                                    <div className="itemValue">janedow@gmail.com</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="right"></div>
